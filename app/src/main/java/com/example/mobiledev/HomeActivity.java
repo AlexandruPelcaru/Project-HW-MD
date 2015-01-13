29cb316d704478267eab759c7beed7fdf8bfde1d
Vlad POPESCU <v1_ad@yahoo.com>
2015-01-13 19:43:50 +0200

    register,home

--- app/src/main/java/com/example/mobiledev/HomeActivity.java
+++ app/src/main/java/com/example/mobiledev/HomeActivity.java
@@ -1,39 +1,147 @@
 package com.example.mobiledev;
 
-import android.app.Activity;
-import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import java.util.ArrayList;
+import java.util.List;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import android.app.Activity;
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.net.Uri;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.parse.ParseException;
+import com.parse.ParseObject;
+import com.parse.ParseQuery;
+import com.parse.ParseUser;
 
 public class HomeActivity extends Activity {
+    String baseURL = "http://api.openweathermap.org/data/2.5/weather?q=";
+    String endBaseURL = "&units=metric";
+    private EditText etCity;
+    private TextView tvWeather;
+    private Button btnRequest, btnProfile;
+    private ListView lvLinks;
+    boolean check = false;
+    JSONParse asyncTask;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        tvWeather = (TextView) findViewById(R.id.tvWeather);
+        etCity = (EditText) findViewById(R.id.etCity);
+
+        btnRequest = (Button) findViewById(R.id.btnGetWeather);
+        btnProfile = (Button) findViewById(R.id.btnProfile);
+        lvLinks = (ListView) findViewById(R.id.lvLinkuri);
+        tvWeather.setText("Current Degrees: Not requested");
+
+        btnRequest.setOnClickListener(new OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                String etCityString=etCity.getText().toString().trim();
+                if (!etCityString.equals("")) {
+                    baseURL = baseURL + (etCity.getText().toString())
+                            + endBaseURL;
+
+                    check = true;
+                }
+                if (check == true) {
+                    if (isNetworkAvailable() == true) {
+                        asyncTask = (JSONParse) new JSONParse().execute();
+                    } else {
+                        tvWeather.setText("No Internet Connection");
+                    }
+                } else {
+                    Toast.makeText(getApplicationContext(), "No city inserted",
+                            Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+
+        btnProfile.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(HomeActivity.this,
+                        ProfileActivity.class));
+            }
+        });
+
+        lvLinks.setOnItemClickListener(new OnItemClickListener() {
+
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view,
+                                    int position, long id) {
+
+
+            }
+        });
+
     }
 
+    private class JSONParse extends AsyncTask<String, String, JSONObject> {
+        private ProgressDialog pDialog;
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_home, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            pDialog = new ProgressDialog(HomeActivity.this);
+            pDialog.setMessage("Getting data...");
+            pDialog.show();
         }
 
-        return super.onOptionsItemSelected(item);
+        @Override
+        protected JSONObject doInBackground(String... args) {
+            JSONParser jParser = new JSONParser();
+            // Getting JSON from URL
+            JSONObject json = jParser.getJSONFromUrl(baseURL);
+            return json;
+        }
+
+        @Override
+        protected void onPostExecute(JSONObject json) {
+            pDialog.dismiss();
+            try {
+                JSONArray array = new JSONArray();
+                json.toJSONArray(array);
+                JSONObject x = json.getJSONObject("main");
+                tvWeather.setText("Current Degrees: " + x.getString("temp")+"°C");
+
+            } catch (JSONException e) {
+                tvWeather.setText("Current Degrees: Not Available");
+                e.printStackTrace();
+            }
+        }
+
     }
+
+    private boolean isNetworkAvailable() {
+        ConnectivityManager connectivityManager = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo activeNetworkInfo = connectivityManager
+                .getActiveNetworkInfo();
+        return activeNetworkInfo != null && activeNetworkInfo.isConnected();
+    }
+
+
 }
--- app/src/main/java/com/example/mobiledev/RegisterActivity.java
+++ app/src/main/java/com/example/mobiledev/RegisterActivity.java
@@ -1,39 +1,78 @@
 package com.example.mobiledev;
 
+
 import android.app.Activity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import com.parse.ParseException;
+import com.parse.ParseUser;
+import com.parse.SaveCallback;
+import com.parse.SignUpCallback;
 
 public class RegisterActivity extends Activity {
+    private EditText etUsername, etPassword;
+    private Button btnRegister;
+    private boolean registerSuccesful;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        etUsername = (EditText) findViewById(R.id.etUsernameRegister);
+        etPassword = (EditText) findViewById(R.id.etPasswordRegister);
+        btnRegister = (Button) findViewById(R.id.btnRegisterRegister);
+
+        btnRegister.setOnClickListener(new OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                onRegisterPressed(getUsername(), getPassword());
+            }
+        });
     }
 
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_register, menu);
-        return true;
+    public String getUsername() {
+        return etUsername.getEditableText().toString();
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+    public String getPassword() {
+        return etPassword.getEditableText().toString();
+    }
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+    public void onRegisterPressed(String username, String password) {
+        ParseUser user = new ParseUser();
+        //Log.i("TAG", "da click");
+        user.setUsername(username);
+        user.setPassword(password);
+        user.put("Password",password);
+        user.signUpInBackground(new SignUpCallback() {
+
+            @Override
+            public void done(ParseException e) {
+                if (e == null) {
+                    onRegisterFinished("");
+                } else {
+                    onRegisterFinished(e.getMessage());
+                }
+
+            }
+        });
+    }
+
+    public void onRegisterFinished(String error) {
+        if (error.equals("")) {
+
+            finish();
+        } else {
+            Toast.makeText(getApplicationContext(), error, Toast.LENGTH_LONG)
+                    .show();
         }
-
-        return super.onOptionsItemSelected(item);
     }
 }
